2018d51900cd32107eeace5ab329b5ad09ef140e
toffan <benoit.lemarchand@etu.enseeiht.fr>
2015-02-03 23:43:49 +0000

    Ajout de templates
    L'usage du type calc_t a été enlevé dans mathc.cpp pour laisser la place
    à des templates

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	g++ -Wall -Wextra -pedantic -std=c++14 -lsfml-graphics -lsfml-system -lsfml-window -o window main.cpp maths.cpp window.cpp clo.cpp
+	g++ -Wall -Wextra -pedantic -std=c++14 -lsfml-graphics -lsfml-system -lsfml-window -o window main.cpp window.cpp clo.cpp
 
 release:
-	g++ -O3 -Wall -Wextra -pedantic -std=c++14 -lsfml-graphics -lsfml-system -lsfml-window -o window main.cpp maths.cpp window.cpp clo.cpp
+	g++ -O3 -Wall -Wextra -pedantic -std=c++14 -lsfml-graphics -lsfml-system -lsfml-window -o window main.cpp window.cpp clo.cpp
--- maths.hpp
+++ maths.hpp
@@ -6,22 +6,71 @@
 #include <SFML/Graphics/Image.hpp> //sf::Image
 #include <SFML/System/Vector2.hpp> //sf::Vector2
 
-using calc_t = double;
 
+template<class T>
 std::size_t algo_color(
-        std::complex<calc_t> z,
-        const std::complex<calc_t> julia_cst,
+        std::complex<T> z,
+        const std::complex<T> julia_cst,
         std::size_t color_max);
 
-std::complex<calc_t> affixe_rel(
-        sf::Vector2<calc_t> pix_pos,
-        sf::Vector2<calc_t> win_pix_size,
-        sf::Vector2<calc_t> win_math_size);
+template<class T>
+std::complex<T> affixe_rel(
+        sf::Vector2<T> pix_pos,
+        sf::Vector2<T> win_pix_size,
+        sf::Vector2<T> win_math_size);
 
+template<class T>
 void generate_fractal(
         sf::Image &fractal,
         const std::vector<sf::Color> &colors_table,
-        const std::complex<calc_t> jula_cst);
+        const std::complex<T> jula_cst);
 
+
+
+template<class T>
+std::size_t algo_color(
+        std::complex<T> z,
+        const std::complex<T> julia_cst,
+        std::size_t color_max) {
+    std::size_t color{0};
+
+    while(color < color_max && norm(z) < 4){
+        z = z * z + julia_cst;
+        ++color;
+    }
+
+    return color;
+}
+
+
+template<class T>
+std::complex<T> affixe_rel(
+        sf::Vector2<T> pix_pos,
+        sf::Vector2<T> win_pix_size,
+        sf::Vector2<T> win_math_size) {
+    return {
+            (pix_pos.x - win_pix_size.x/2) * win_math_size.x / win_pix_size.x,
+            (win_pix_size.y/2 - pix_pos.y) * win_math_size.y / win_pix_size.y};
+}
+
+
+template<class T>
+void generate_fractal(
+        sf::Image &fractal,
+        const std::vector<sf::Color> &colors_table,
+        const std::complex<T> julia_cst) {
+    std::complex<T> affixe;
+    std::size_t color;
+    for(std::size_t i(0) ; i!=fractal.getSize().x ; ++i){
+        for(std::size_t j(0) ; j!=fractal.getSize().y ; ++j){
+            affixe = affixe_rel(
+                    sf::Vector2<T>(i,j),
+                    sf::Vector2<T>(fractal.getSize()),
+                    sf::Vector2<T>(2.4, 2.4));
+            color = algo_color(affixe, julia_cst, colors_table.size()-1);
+            fractal.setPixel(i, j, colors_table[color]);
+        }
+    }
+}
 
 #endif // MATHS_H_INCLUDED
